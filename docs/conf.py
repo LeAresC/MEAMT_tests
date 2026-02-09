1962fda415253726bc2e1dcc960118cb3c87143d
Luis <>
2026-02-09 14:48:58 -0300

    Teste

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax', # Opcional: para renderizar fórmulas matemáticas
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
